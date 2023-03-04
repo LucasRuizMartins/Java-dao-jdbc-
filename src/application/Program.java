8b1cf570070db27d1150c512d7e550ae2664554b
Lucas Ruiz <deth_lucas@hotmail.com>
2023-03-04 11:16:26 -0300

    update

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,7 +38,17 @@ public class Program {
 			System.out.println("\n === TEST 4 : Seller Insert");
 			Seller newSeller = new Seller (null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 			sellerDao.insert(newSeller);
-			System.out.println("Inserted ! new id = " newSeller.getId());
+			System.out.println("Inserted ! new id = " + newSeller.getId());
+			
+			
+
+			System.out.println("\n === TEST 5 : Seller Update");
+			
+			seller = sellerDao.findById(1); 
+			seller.setName("Martha Waine");
+			sellerDao.update(seller);
+			System.out.println("update completed");
+			
 		}
 }
 
