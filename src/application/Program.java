4a585151b6d64b2698e496155743895556be7ca4
Lucas Ruiz <deth_lucas@hotmail.com>
2023-03-04 11:22:34 -0300

    delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,7 +13,7 @@ public class Program {
 
 	
 		public static void main (String[] args) {
-			
+			Scanner sc = new Scanner(System.in);
 			SellerDao sellerDao = DaoFactory.createSellerDao();
 			
 			System.out.println("=== TEST 1 : seller FindByID");
@@ -48,7 +49,14 @@ public class Program {
 			seller.setName("Martha Waine");
 			sellerDao.update(seller);
 			System.out.println("update completed");
+		
+			System.out.println("\n === TEST 6 : Seller Delete");
+			System.out.println("ENTRE COM O ID");
+			int deleteId = sc.nextInt();
+			sellerDao.deleteById(deleteId);
+			System.out.println("Delete completed");
 			
+			sc.close();
 		}
 }
 
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -26,68 +26,77 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void insert(Seller obj) {
-		PreparedStatement st = null; 
+		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement( "  \r\n"
-					+ "            INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) \r\n"
-					+ "            VALUES (?, ?, ? , ? , ?  )", 
+			st = conn.prepareStatement(
+					"  \r\n" + "            INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) \r\n"
+							+ "            VALUES (?, ?, ? , ? , ?  )",
 					Statement.RETURN_GENERATED_KEYS);
-			
+
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
- 
-			
+
 			int rowsAffected = st.executeUpdate();
-			
+
 			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys(); 
-				if(rs.next()) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
 				DB.closeResultSet(rs);
-			}
-			else {throw new DbException("Unexpected error ! no Rows affected");
+			} else {
+				throw new DbException("Unexpected error ! no Rows affected");
 			}
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
 		}
-				
+
 	}
 
 	@Override
 	public void update(Seller obj) {
-		PreparedStatement st = null; 
+		PreparedStatement st = null;
 		try {
 			st = conn.prepareStatement("UPDATE seller \r\n"
 					+ "SET Name = ? , Email = ? , BirthDate = ?, BaseSalary = ?, DepartmentId = ? \r\n"
-					+ "Where Id = ?"  );
-			
+					+ "Where Id = ?");
+
 			st.setString(1, obj.getName());
 			st.setString(2, obj.getEmail());
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
 			st.setInt(6, obj.getId());
- 
+
 			st.executeUpdate();
-						
+
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
 			DB.closeStatement(st);
-		}				
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			st.setInt(1, id);
+			st.executeUpdate();
 
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally  {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
